375904337c6553f7690853ed07ebb33723344f9f
tomzacchia <tomzacchia@live.com>
2019-11-04 16:16:09 -0500

    Add event handler for search submit btn

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,35 @@
 import axios from 'axios';
 import Search from './models/Search';
 
-const search = new Search('pizza');
+/* Global sate of the app 
+  - search object
+  - current recipe object
+  - shopping list object
+  - liked recipes
+*/
 
-search.getResults();
+const state = {};
+
+const searchHandler = async () => {
+  // 1. Get query from view
+  const query = 'pizza';
+
+  if (query) {
+    // 2. new search object and add it to state
+    state.search = new Search(query);
+
+    // 3. prepare UI for results
+
+    // 4. search results for recipes
+    await state.search.getResults();
+
+    // 5. render results on UI
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  searchHandler();
+})
+
